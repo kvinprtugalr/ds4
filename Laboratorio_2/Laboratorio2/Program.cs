736c6081e432bc8546acd095280a234bdf705823
Kevin Portugal <kvinprtugalr@gmail.com>
2025-09-15 19:41:02 -0500

    Agrego carpeta Laboratorio_2

---
+++ Laboratorio_2/Laboratorio2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Declarando variables de instancia en C#
+using System;
+using System.Net.Http.Json;
+
+namespace Laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            //Ejemplo utilizando las variables de instancia de Clase.
+            client.FirstName = "Kevin";
+            client.LastName = "Portugal";
+            client.Age = 15;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+
+        }
+
+        public class Client
+        {
+            //Declarando variables de instancia en clase.
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public ushort Age { get; set; }
+
+            public string GetFullName()
+            {
+                //Utilizando variables de instancia dentro de metodos de clase.
+                return FirstName + " " + LastName;
+            }
+        }
+    }
+}
+
---
+++ Laboratorio_2/Laboratorio22/Program.cs
@@ -0,0 +1,22 @@
+﻿// Accediendo a variable de clase o estatica en C#.
+using System;
+
+namespace Laboratorio22
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //Asignando valor a variable estatica.
+            MyClass.Valor = 1;
+            Console.WriteLine(MyClass.Valor);
+
+        }
+    }
+
+    public class MyClass
+    {
+        //Declarando variable estatica
+        public static int Valor;
+    }
+}
