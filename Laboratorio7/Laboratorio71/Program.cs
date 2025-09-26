0b9e91aaad0d075a541cdd7e2b62498b7a3dc1a0
Kevin Portugal <kvinprtugalr@gmail.com>
2025-09-25 22:21:23 -0500

    Se agrego el Laboratorio7

---
+++ Laboratorio7/Laboratorio71/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Laboratorio71
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco banco1 = new Banco();
+            banco1.Operar();
+            banco1.DepositosTotales();
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Laboratorio7/Laboratorio72/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Laboratorio72
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JuegoDeDados j = new JuegoDeDados();
+            j.Jugar();
+            
+        }
+    }
+}
