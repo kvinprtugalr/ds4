4df26aae6bc1e6f07ba104f2bbbaa20b629ec6c4
Kevin Portugal <kvinprtugalr@gmail.com>
2025-11-22 00:53:52 -0500

    Se agrega Lab18, se trabaja en carpeta Lab181

---
+++ Laboratorio18/Laboratorio181/Controllers/AccessController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio181.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Enter(String user, String password)
+        {
+            try
+            {
+                return Content("1");
+            }
+            catch (Exception ex)
+            {
+                return Content("Ocurio un error: (" + ex.Message);
+
+            }
+        }
+    }
+}
